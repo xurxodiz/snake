5e310a3a5be1b4a8b0e3e86886e42c20185517f6
Laurent Dufour <laurent.dufour33@gmail.com>
2015-03-22 00:18:30 +0100

    bike style and img + tail style

--- public/js/drawableUtil.js
+++ public/js/drawableUtil.js
@@ -6,10 +6,27 @@ export class DrawableUtil {
         this.ctx = ctx;
     }
 
+
     rect(color, x, y, width, height) {
         this.ctx.fillStyle = color;
         this.ctx.beginPath();
         this.ctx.rect(x, y, width, height);
+        this.ctx.closePath();
+        this.ctx.fill();
+    }
+
+    tail(color, x, y, width, height) {
+        var gradient=this.ctx.createLinearGradient(x+5,y,x+5,y+10);
+        gradient.addColorStop(0,"transparent");
+        gradient.addColorStop(0.3,"transparent");
+        gradient.addColorStop(0.4,"#9bdcd8");
+        gradient.addColorStop(0.6,"#9bdcd8");
+        gradient.addColorStop(0.7,"transparent");
+        gradient.addColorStop(1,"transparent");
+        this.ctx.fillStyle = gradient;
+        this.ctx.beginPath();
+        this.ctx.rect(x, y, width, height);
+        this.ctx.fillRect(x, y, width, height);
         this.ctx.closePath();
         this.ctx.fill();
     }
@@ -20,5 +37,44 @@ export class DrawableUtil {
         this.ctx.arc(x, y, radius, 0, Math.PI*2, true);
         this.ctx.closePath();
         this.ctx.fill();
+    }
+/*
+0 left
+1 up
+2 right
+3 bottom
+*/
+    bike(x, y, angle) {
+        var degrees = 0;
+        switch (angle) {
+            case 0:
+                degrees = -90;
+                break; 
+            case 1:
+                degrees = 0;
+                break;  
+            case 2:
+                degrees = 90;
+                break;  
+            case 3:
+                degrees = 180;
+                break; 
+            default: 
+                degrees = 0;
+        }
+
+        var img = new Image();
+        img.src = '../images/trake_bike.png';
+
+        this.ctx.save();
+
+        this.ctx.translate(x, y);
+        this.ctx.translate(5, 5);
+
+        this.ctx.rotate(degrees*Math.PI/180);
+
+        this.ctx.drawImage(img, -5, -28, 10, 33);
+
+        this.ctx.restore();
     }
 }
--- public/js/snakeView.js
+++ public/js/snakeView.js
@@ -13,7 +13,9 @@ export class SnakeView {
     draw() {
         this.entity.positions.forEach((position) => {
             var {x, y} = position;
-            this.drawableUtil.rect("#FFFFFF", x, y, this.entity.dx, this.entity.dy);
+            this.drawableUtil.tail("#FFFFFF", x, y, this.entity.dx, this.entity.dy);
         });
+        var {x, y} = this.entity.positions[0];
+        this.drawableUtil.bike(x, y, this.entity.direction);
     }
 }
