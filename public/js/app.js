b952187301229817b226354d8f7ae6957a4a7b25
Romain Maneschi <romain@maneschi.fr>
2015-03-21 23:22:43 +0100

    default init size of snake controlled by keyboard to 50

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,6 @@
 import {Game} from './game';
 
 setTimeout(function() {
-    new Game({nbFood: 100, controller: 'KeyboardController', snakeInitSize: 10}).run();
+    new Game({nbFood: 100, controller: 'KeyboardController', snakeInitSize: 50}).run();
     //new Game({nbFood: 5, controller: 'IAController', snakeInitSize: 5}).run();
 }, 3000);
